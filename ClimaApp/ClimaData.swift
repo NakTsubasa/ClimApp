e7a212dfb6acc5f4b075a923bc0afb4971f47aba
edith garcia ziranda <edithgarciaziranda@gmail.com>
2020-11-30 04:54:16 -0600

    Creacion de los demas archivos

---
+++ ClimaApp/ClimaData.swift
@@ -0,0 +1,9 @@
+//
+//  ClimaData.swift
+//  ClimaApp
+//
+//  Created by Mac7 on 30/11/20.
+//  Copyright © 2020 itmorelia. All rights reserved.
+//
+
+import Foundation
---
+++ ClimaApp/ClimaModelo.swift
@@ -0,0 +1,9 @@
+//
+//  ClimaModelo.swift
+//  ClimaApp
+//
+//  Created by Mac7 on 30/11/20.
+//  Copyright © 2020 itmorelia. All rights reserved.
+//
+
+import Foundation
